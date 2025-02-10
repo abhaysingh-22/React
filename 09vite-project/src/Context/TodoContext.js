457b5c94450fa39681af4709f3d93477ba281a9b
myfirstrepo <abhaysingh6575@gmail.com>
2025-02-10 19:30:35 +0530

    todo commit

---
+++ 09vite-project/src/Context/TodoContext.js
@@ -0,0 +1,7 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext();
+
+export const useTodo = () => useContext(TodoContext);
+
+export const TodoProvider = TodoContext.Provider;
